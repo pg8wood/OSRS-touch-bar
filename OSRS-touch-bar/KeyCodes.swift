50052d9e4008215a4bec844d589249be4b03706f
Patrick Gatewood <pg8wood@vt.edu>
2018-02-26 15:05:08 -0500

    Create keypress events

---
+++ OSRS-touch-bar/KeyCodes.swift
@@ -0,0 +1,23 @@
+//
+//  KeyCodes.swift
+//  OSRS-touch-bar
+//
+//  Created by Patrick Gatewood on 2/26/18.
+//  Copyright © 2018 Patrick Gatewood. All rights reserved.
+//
+
+class KeyCodes {
+    // CGKeyCode in CoreGraphics is a typealias for UInt16
+    static let F1KeyCode: UInt16 = 122
+    static let F2KeyCode: UInt16 = 120
+    static let F3KeyCode: UInt16 = 99
+    static let F4KeyCode: UInt16 = 118
+    static let F5KeyCode: UInt16 = 96
+    static let F6KeyCode: UInt16 = 97
+    static let F7KeyCode: UInt16 = 98
+    static let F8KeyCode: UInt16 = 100
+    static let F9KeyCode: UInt16 = 101
+    static let F10KeyCode: UInt16 = 109
+    static let F11KeyCode: UInt16 = 103
+    static let F12KeyCode: UInt16 = 111
+}
--- OSRS-touch-bar/ViewController.swift
+++ OSRS-touch-bar/ViewController.swift
@@ -25,9 +25,35 @@ class ViewController: NSViewController {
     @IBOutlet var musicButton: NSButton!
     
     let osrsInterfaceIdentifiers: [NSTouchBarItem.Identifier] = [.combatOptionsLabelItem, .statsLabelItem, .questListLabelItem, .inventoryLabelItem, .equipmentLabelItem, .prayerLabelItem, .spellbookLabelItem, .clanChatLabelItem, .friendsListLabelItem, .ignoreListLabelItem, .optionsLabelItem, .emotesLabelItem, .musicPlayerLabelItem]
+    
+    
+    var keyCodeDict: [NSButton: UInt16]?
+    
+    // MARK: - View life cycle
+    
+    // Map Touch Bar buttons to function buttons
+    override func viewDidLoad() {
+        keyCodeDict = [combatOptionsButton: KeyCodes.F1KeyCode]
+    }
 
 
+    // Detects a Touch Bar button press and sends the corresponding function key press event
     @IBAction func buttonPressed(sender: NSButton) {
+        guard let keyCode: UInt16 = keyCodeDict?[sender],
+            let keyDownEvent: CGEvent = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: true),
+            let keyUpEvent: CGEvent = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: false)
+            else {
+                return
+        }
+        
+        // Indicate that these are keyboard events
+        keyDownEvent.flags = CGEventFlags.maskCommand
+        keyUpEvent.flags = CGEventFlags.maskCommand
+        
+        // Post both events
+        keyDownEvent.post(tap: CGEventTapLocation.cghidEventTap)
+        keyUpEvent.post(tap: CGEventTapLocation.cghidEventTap)
+        
         NSAlert().runModal()
     }
 }
@@ -42,12 +68,5 @@ extension ViewController: NSTouchBarDelegate {
         return touchBarOutlet
     }
     
-//    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
-//        let viewItem = NSCustomTouchBarItem(identifier: identifier)
-////        viewItem.view = NSImageView(image: NSImage(named: NSImage.Name(rawValue: "combat-options"))!)
-//
-//
-//        return nil
-//    }
 }
 
