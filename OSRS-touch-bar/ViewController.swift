bdd372175b08dd6c4d36c065b0f48bae7b28664f
Patrick Gatewood <pg8wood@vt.edu>
2018-02-27 18:21:12 -0500

    Create keypress AppleScript
    Sends a system-wide function key press when the combat-options
    button is pressed

--- OSRS-touch-bar/ViewController.swift
+++ OSRS-touch-bar/ViewController.swift
@@ -26,34 +26,38 @@ class ViewController: NSViewController {
     
     let osrsInterfaceIdentifiers: [NSTouchBarItem.Identifier] = [.combatOptionsLabelItem, .statsLabelItem, .questListLabelItem, .inventoryLabelItem, .equipmentLabelItem, .prayerLabelItem, .spellbookLabelItem, .clanChatLabelItem, .friendsListLabelItem, .ignoreListLabelItem, .optionsLabelItem, .emotesLabelItem, .musicPlayerLabelItem]
     
-    
     var keyCodeDict: [NSButton: UInt16]?
+
     
     // MARK: - View life cycle
     
     // Map Touch Bar buttons to function buttons
-    //override func viewDidLoad() {
-      //  keyCodeDict = [combatOptionsButton: KeyCodes.F1KeyCode]
-    //}
+    override func viewDidLoad() {
+        keyCodeDict = [combatOptionsButton: KeyCodes.F1KeyCode]
+    }
 
     // Detects a Touch Bar button press and sends the corresponding function key press event
     @IBAction func buttonPressed(sender: NSButton) {
-        guard let keyCode: UInt16 = keyCodeDict?[sender],
-            let keyDownEvent: CGEvent = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: true),
-            let keyUpEvent: CGEvent = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: false)
-            else {
+        var scriptError: NSDictionary?
+        guard let keyCode: UInt16 = keyCodeDict?[sender] else {
                 return
         }
         
-        // Indicate that these are keyboard events
-        keyDownEvent.flags = CGEventFlags.maskCommand
-        keyUpEvent.flags = CGEventFlags.maskCommand
+        let keycodeScriptSource = """
+        tell application "System Events" to key code \(keyCode)
+        """
         
-        // Post both events
-        keyDownEvent.post(tap: CGEventTapLocation.cghidEventTap)
-        keyUpEvent.post(tap: CGEventTapLocation.cghidEventTap)
+        print(keycodeScriptSource)
         
-        NSAlert().runModal()
+        if let script = NSAppleScript(source: keycodeScriptSource) {
+            script.executeAndReturnError(&scriptError)
+        }
+        
+        if scriptError != nil {
+            let alert = NSAlert()
+            alert.informativeText = "\(String(describing: scriptError!.allValues))"
+            alert.runModal()
+        }
     }
 }
 
